cee96fecacdf1ca686a831e1f6afe0fd047e760a
Satoshi Brasileiro <102581645+chiaraberti13@users.noreply.github.com>
2025-10-05 22:33:03 +0200

    Add stricter FotoArtPuzzle front validations

--- modules/fotoartpuzzle/controllers/front/preview.php
+++ modules/fotoartpuzzle/controllers/front/preview.php
@@ -19,9 +19,11 @@ class FotoartpuzzlePreviewModuleFrontController extends ModuleFrontController
         try {
             $this->validateToken();
             $file = Tools::getValue('file');
-            $text = Tools::getValue('box_text', '');
+            $text = $this->sanitizeBoxText(Tools::getValue('box_text', ''));
             $color = Tools::getValue('box_color', '#FFFFFF');
-            $font = Tools::getValue('box_font', 'Roboto-Regular');
+            $font = (string) Tools::getValue('box_font', 'Roboto');
+
+            $this->validateBoxOptions($text, $color, $font);
 
             if (!$file || !file_exists($file)) {
                 throw new Exception($this->module->l('File not found.'));
@@ -67,5 +69,52 @@ class FotoartpuzzlePreviewModuleFrontController extends ModuleFrontController
         $renderer->render($file, $text, $color, $font, $destination);
 
         return $destination;
+    }
+
+    private function sanitizeBoxText($text)
+    {
+        $text = trim((string) $text);
+        $text = strip_tags($text);
+        $text = preg_replace('/\s+/u', ' ', $text);
+
+        return $text;
+    }
+
+    private function validateBoxOptions($text, &$color, &$font)
+    {
+        $config = FAPConfiguration::getFrontConfig();
+        $maxChars = (int) $config['box']['maxChars'];
+        if ($maxChars > 0 && Tools::strlen($text) > $maxChars) {
+            throw new Exception(sprintf($this->module->l('The box text cannot exceed %d characters.'), $maxChars));
+        }
+
+        $allowedColors = array_map([$this, 'normalizeColor'], $config['box']['colors']);
+        $normalizedColor = $this->normalizeColor($color);
+        if (!in_array($normalizedColor, $allowedColors, true)) {
+            throw new Exception($this->module->l('Selected color is not available.'));
+        }
+
+        $allowedFonts = array_map('trim', $config['box']['fonts']);
+        $normalizedFont = trim((string) $font);
+        if (!in_array($normalizedFont, $allowedFonts, true)) {
+            throw new Exception($this->module->l('Selected font is not available.'));
+        }
+
+        $color = $normalizedColor;
+        $font = $normalizedFont;
+    }
+
+    private function normalizeColor($color)
+    {
+        $color = trim((string) $color);
+        if ($color === '') {
+            return $color;
+        }
+
+        if ($color[0] !== '#') {
+            $color = '#' . $color;
+        }
+
+        return '#' . Tools::strtoupper(ltrim($color, '#'));
     }
 }

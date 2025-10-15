9d9eb58167c2b8da8e4dda1e0075c760dab5ef9b
Satoshi Brasileiro <102581645+chiaraberti13@users.noreply.github.com>
2025-10-15 11:19:24 +0200

    Fix install SQL queries to allow module installation

--- puzzlecustomizer/sql/install.php
+++ puzzlecustomizer/sql/install.php
@@ -1,16 +1,98 @@
 <?php
+
 class PuzzleCustomizerSqlInstall
 {
     public static function install()
     {
         $queries = [
-            'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . "puzzle_product` (\n                `id_puzzle_product` INT UNSIGNED NOT NULL AUTO_INCREMENT,\n                `id_product` INT UNSIGNED NOT NULL,\n                `active` TINYINT(1) NOT NULL DEFAULT 1,\n                PRIMARY KEY (`id_puzzle_product`),\n                UNIQUE KEY `idx_product` (`id_product`)\n            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;',
-            'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . "puzzle_option` (\n                `id_puzzle_option` INT UNSIGNED NOT NULL AUTO_INCREMENT,\n                `name` VARCHAR(128) NOT NULL,\n                `width_mm` DECIMAL(10,2) NULL,\n                `height_mm` DECIMAL(10,2) NULL,\n                `pieces` INT NULL,\n                `price_impact` DECIMAL(20,6) NULL,\n                `active` TINYINT(1) NOT NULL DEFAULT 1,\n                PRIMARY KEY (`id_puzzle_option`)\n            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;',
-            'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . "puzzle_font` (\n                `id_puzzle_font` INT UNSIGNED NOT NULL AUTO_INCREMENT,\n                `name` VARCHAR(128) NOT NULL,\n                `file` VARCHAR(128) NOT NULL,\n                `preview` VARCHAR(128) NULL,\n                `active` TINYINT(1) NOT NULL DEFAULT 1,\n                PRIMARY KEY (`id_puzzle_font`)\n            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;',
-            'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . "puzzle_image_format` (\n                `id_puzzle_image_format` INT UNSIGNED NOT NULL AUTO_INCREMENT,\n                `name` VARCHAR(64) NOT NULL,\n                `extensions` VARCHAR(255) NULL,\n                `max_size` INT NULL,\n                `mime_types` VARCHAR(255) NULL,\n                `active` TINYINT(1) NOT NULL DEFAULT 1,\n                PRIMARY KEY (`id_puzzle_image_format`)\n            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;',
-            'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . "puzzle_box_color` (\n                `id_puzzle_box_color` INT UNSIGNED NOT NULL AUTO_INCREMENT,\n                `name` VARCHAR(64) NOT NULL,\n                `hex` VARCHAR(7) NOT NULL,\n                `active` TINYINT(1) NOT NULL DEFAULT 1,\n                PRIMARY KEY (`id_puzzle_box_color`)\n            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;',
-            'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . "puzzle_text_color` (\n                `id_puzzle_text_color` INT UNSIGNED NOT NULL AUTO_INCREMENT,\n                `name` VARCHAR(64) NOT NULL,\n                `hex` VARCHAR(7) NOT NULL,\n                `active` TINYINT(1) NOT NULL DEFAULT 1,\n                PRIMARY KEY (`id_puzzle_text_color`)\n            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;',
-            'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . "puzzle_customization` (\n                `id_puzzle_customization` INT UNSIGNED NOT NULL AUTO_INCREMENT,\n                `id_cart` INT UNSIGNED NULL,\n                `id_order` INT UNSIGNED NULL,\n                `token` VARCHAR(64) NOT NULL,\n                `configuration` LONGTEXT NULL,\n                `image_path` VARCHAR(255) NULL,\n                `status` VARCHAR(32) NULL,\n                `created_at` DATETIME NULL,\n                `updated_at` DATETIME NULL,\n                PRIMARY KEY (`id_puzzle_customization`)\n            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;',
+            sprintf(
+                'CREATE TABLE IF NOT EXISTS `%1$spuzzle_product` (
+                    `id_puzzle_product` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    `id_product` INT UNSIGNED NOT NULL,
+                    `active` TINYINT(1) NOT NULL DEFAULT 1,
+                    PRIMARY KEY (`id_puzzle_product`),
+                    UNIQUE KEY `idx_product` (`id_product`)
+                ) ENGINE=%2$s DEFAULT CHARSET=utf8;',
+                _DB_PREFIX_,
+                _MYSQL_ENGINE_
+            ),
+            sprintf(
+                'CREATE TABLE IF NOT EXISTS `%1$spuzzle_option` (
+                    `id_puzzle_option` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    `name` VARCHAR(128) NOT NULL,
+                    `width_mm` DECIMAL(10,2) NULL,
+                    `height_mm` DECIMAL(10,2) NULL,
+                    `pieces` INT NULL,
+                    `price_impact` DECIMAL(20,6) NULL,
+                    `active` TINYINT(1) NOT NULL DEFAULT 1,
+                    PRIMARY KEY (`id_puzzle_option`)
+                ) ENGINE=%2$s DEFAULT CHARSET=utf8;',
+                _DB_PREFIX_,
+                _MYSQL_ENGINE_
+            ),
+            sprintf(
+                'CREATE TABLE IF NOT EXISTS `%1$spuzzle_font` (
+                    `id_puzzle_font` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    `name` VARCHAR(128) NOT NULL,
+                    `file` VARCHAR(128) NOT NULL,
+                    `preview` VARCHAR(128) NULL,
+                    `active` TINYINT(1) NOT NULL DEFAULT 1,
+                    PRIMARY KEY (`id_puzzle_font`)
+                ) ENGINE=%2$s DEFAULT CHARSET=utf8;',
+                _DB_PREFIX_,
+                _MYSQL_ENGINE_
+            ),
+            sprintf(
+                'CREATE TABLE IF NOT EXISTS `%1$spuzzle_image_format` (
+                    `id_puzzle_image_format` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    `name` VARCHAR(64) NOT NULL,
+                    `extensions` VARCHAR(255) NULL,
+                    `max_size` INT NULL,
+                    `mime_types` VARCHAR(255) NULL,
+                    `active` TINYINT(1) NOT NULL DEFAULT 1,
+                    PRIMARY KEY (`id_puzzle_image_format`)
+                ) ENGINE=%2$s DEFAULT CHARSET=utf8;',
+                _DB_PREFIX_,
+                _MYSQL_ENGINE_
+            ),
+            sprintf(
+                'CREATE TABLE IF NOT EXISTS `%1$spuzzle_box_color` (
+                    `id_puzzle_box_color` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    `name` VARCHAR(64) NOT NULL,
+                    `hex` VARCHAR(7) NOT NULL,
+                    `active` TINYINT(1) NOT NULL DEFAULT 1,
+                    PRIMARY KEY (`id_puzzle_box_color`)
+                ) ENGINE=%2$s DEFAULT CHARSET=utf8;',
+                _DB_PREFIX_,
+                _MYSQL_ENGINE_
+            ),
+            sprintf(
+                'CREATE TABLE IF NOT EXISTS `%1$spuzzle_text_color` (
+                    `id_puzzle_text_color` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    `name` VARCHAR(64) NOT NULL,
+                    `hex` VARCHAR(7) NOT NULL,
+                    `active` TINYINT(1) NOT NULL DEFAULT 1,
+                    PRIMARY KEY (`id_puzzle_text_color`)
+                ) ENGINE=%2$s DEFAULT CHARSET=utf8;',
+                _DB_PREFIX_,
+                _MYSQL_ENGINE_
+            ),
+            sprintf(
+                'CREATE TABLE IF NOT EXISTS `%1$spuzzle_customization` (
+                    `id_puzzle_customization` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    `id_cart` INT UNSIGNED NULL,
+                    `id_order` INT UNSIGNED NULL,
+                    `token` VARCHAR(64) NOT NULL,
+                    `configuration` LONGTEXT NULL,
+                    `image_path` VARCHAR(255) NULL,
+                    `status` VARCHAR(32) NULL,
+                    `created_at` DATETIME NULL,
+                    `updated_at` DATETIME NULL,
+                    PRIMARY KEY (`id_puzzle_customization`)
+                ) ENGINE=%2$s DEFAULT CHARSET=utf8;',
+                _DB_PREFIX_,
+                _MYSQL_ENGINE_
+            ),
         ];
 
         foreach ($queries as $query) {
